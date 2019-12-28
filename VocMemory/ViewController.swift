2e7721e40d974f6707b59d0a01c8b796d8b5128a
mendel bellaiche <bellaichemendel@gmail.com>
2019-12-29 01:44:46 +0200

    Initial Commit

---
+++ VocMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VocMemory
+//
+//  Created by mendel bellaiche on 29/12/2019.
+//  Copyright © 2019 mendel bellaiche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
